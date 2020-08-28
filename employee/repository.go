4fc27c5b2e6b7bd695a25ff06a583d09d467f438
patrick paipay <jpatrickpl@gmail.com>
2020-08-28 17:12:34 -0500

    get Employees Paginated

---
+++ employee/repository.go
@@ -0,0 +1,59 @@
+package employee
+
+import (
+	"database/sql"
+
+	"github.com/GolangNorthwindRestApi/helper"
+)
+
+type Repository interface {
+	GetEmployees(params *getEmployeesRequest) ([]*Employee, error)
+	GetTotalEmployees() (int64, error)
+}
+
+type repository struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) Repository {
+	return &repository{db: db}
+}
+
+func (repo *repository) GetEmployees(params *getEmployeesRequest) ([]*Employee, error) {
+	const sql = `SELECT id, first_name,last_name,
+				 company, email_address, job_title,
+				 business_phone,home_phone,
+				 coalesce (mobile_phone,''), fax_number,address
+				 FROM EMPLOYEES
+				 LIMIT ? OFFSET ?`
+
+	results, err := repo.db.Query(sql, params.Limit, params.Offset)
+	helper.Catch(err)
+
+	var employees []*Employee
+
+	for results.Next() {
+		employee := &Employee{}
+
+		err = results.Scan(&employee.ID, &employee.FirstName, &employee.LastName, &employee.Company,
+			&employee.EmailAddress, &employee.JobTitle, &employee.BusinessPhone, &employee.HomePhone,
+			&employee.MobilePhone, &employee.FaxNumber, &employee.Address)
+		helper.Catch((err))
+		employees = append(employees, employee)
+
+	}
+
+	return employees, nil
+
+}
+
+func (repo *repository) GetTotalEmployees() (int64, error) {
+	const sql = `SELECT COUNT(*) FROM EMPLOYEES`
+	var total int64
+
+	row := repo.db.QueryRow(sql)
+	err := row.Scan(&total)
+	helper.Catch((err))
+	return total, err
+
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/GolangNorthwindRestApi/database"
+	"github.com/GolangNorthwindRestApi/employee"
 	"github.com/GolangNorthwindRestApi/product"
 	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
@@ -13,11 +14,20 @@ func main() {
 
 	Databaseconnection := database.InitDB()
 	defer Databaseconnection.Close()
-	var productRepository = product.NewRepository(Databaseconnection)
-	var productService product.Service
+	var (
+		productRepository  = product.NewRepository(Databaseconnection)
+		employeeRepository = employee.NewRepository(Databaseconnection)
+	)
+	var (
+		productService  product.Service
+		employeeService employee.Service
+	)
 	productService = product.NewService(productRepository)
+	employeeService = employee.NewService(employeeRepository)
+
 	r := chi.NewRouter()
 	r.Mount("/products", product.MakeHttpHandler(productService))
+	r.Mount("/employees", employee.MakeHttpHandler(employeeService))
 	http.ListenAndServe(":3000", r)
 
 }
